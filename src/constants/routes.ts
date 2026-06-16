965db43c57d2f1b83aff8f59460780d061958d05
mohammad <aahrari5@gmail.com>
2026-06-16 10:24:41 +0330

    update structure app file and update readme.md

---
+++ src/constants/routes.ts
@@ -0,0 +1,5 @@
+/* ---------------------------------- Basic --------------------------------- */
+export const API_ROUTES = '/api';
+export const DOCS_ROUTES = '/docs';
+export const SWAGGER_ROUTES = API_ROUTES + DOCS_ROUTES;
+export const BASE_ROUTES = API_ROUTES + DOCS_ROUTES;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,48 @@
-import { Logger } from '@nestjs/common';
+import {
+  API_ROUTES,
+  SWAGGER_ROUTES,
+} from '@constants/routes';
+import {
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(API_ROUTES);
+  app.useGlobalPipes(new ValidationPipe());
+
+  // CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('BMI Calculator')
+    .setDescription('Api For BMI Calculator')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(SWAGGER_ROUTES, app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'BMI Calculator Api',
+  });
+
   await app.listen(process.env.PORT ?? 4001);
 
-  Logger.log(`Start Project With`);
+  Logger.log(`Swagger run with http://localhost:4001${SWAGGER_ROUTES}`);
 }
 bootstrap();
