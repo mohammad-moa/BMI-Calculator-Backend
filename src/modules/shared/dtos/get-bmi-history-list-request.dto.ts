aec2c89d2b4b4240317fcbc561ca9840c3ff0cb0
mohammad <aahrari5@gmail.com>
2026-09-11 16:06:22 +0330

    update get bmi history list dto

--- src/modules/shared/dtos/get-bmi-history-list-request.dto.ts
+++ src/modules/shared/dtos/get-bmi-history-list-request.dto.ts
@@ -1,8 +1,17 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsIn, IsOptional, IsString } from 'class-validator';
 import { AbstractBaseRequestDto } from './abstract-base-request.dto';
+import { BmiStatusEnum } from '../enums';
 
 export class GetBmiHistoryListRequestDto extends AbstractBaseRequestDto {
   @IsString()
   @IsOptional()
   search?: string;
+
+  @IsEnum(BmiStatusEnum)
+  @IsOptional()
+  status?: BmiStatusEnum;
+
+  @IsIn(['list', 'grid'])
+  @IsOptional()
+  layout?: 'list' | 'grid';
 }
